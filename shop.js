c6bc43f68a6dc98021df75cfb659431a980e7714
lhowe15 <lindsay.howe@snhu.edu>
2023-07-30 14:08:35 +0200

    validation

---
+++ shop.js
@@ -0,0 +1,13 @@
+function validateForm() {
+    let x = document.forms["namebox"]["name"].value;
+    if (x == "") {
+        alert("enter your name");
+        return false;
+    }
+}
+function validateForm() {
+    let x = document.forms["phonebox"]["phone number"].value;
+    if (x == "") {
+        alert("enter your phone number");
+    }
+}
